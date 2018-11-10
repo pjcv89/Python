826b15b3b975dec6b7413fcb13a853ab7115eb60
Pablo Campos <pjcv89@gmail.com>
2018-11-10 13:34:46 -0600

    Update Play_Human_vs_QPlayer.py

--- Aprendizaje por Refuerzo/Tic-Tac-Toe/Play_Human_vs_QPlayer.py
+++ Aprendizaje por Refuerzo/Tic-Tac-Toe/Play_Human_vs_QPlayer.py
@@ -3,7 +3,7 @@ import pickle as pickle
 
 from Q_Learning_Tic_Tac_Toe import Game, HumanPlayer, QPlayer
 
-Q = pickle.load(open("Q_epsilon_09_Nepisodes_200000.p", "rb"))
+Q = pickle.load(open("expert_bot.p", "rb"))
 
 player1 = HumanPlayer(mark="X")
 player2 = QPlayer(mark="O", epsilon=0.0)
